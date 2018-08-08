9a41acab3b49f4d82822c3da848d6bf7683cc728
Konrad <konrad.trade@gmail.com>
2018-08-08 14:53:35 +0200

    Added payload handling

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 const http = require('http');
 const url = require('url');
-
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 
@@ -21,14 +21,30 @@ const server = http.createServer(function(req,res){
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
+    // Get the query string as an object
+    const queryStringobject = parsedUrl.query;
+
     // Get the HTTP method
     const method = req.method.toLowerCase();
 
-    // Send the response
-    res.end('Hello Word\n');
+    // Get the headers as an object
+    const headers = req.headers;
 
-    // Log the request path
-    console.log('Reqest recived on path: '+trimmedPath + ' with this method: ' + method);
+    // Get the payload if there is any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => buffer += decoder.write(data));
+    req.on('end', () => {
+
+        buffer += decoder.end()
+
+        // Send the response
+        res.end('Hello Word\n');
+
+        // Log the request path
+        console.log('Req recievied with this payload', buffer);
+    });
+
 
 })
 
