2c6f3d524c3f4bfad46ace93ed11dc0fe5bf703c
konradovsky <konrad.trade@gmail.com>
2018-11-18 23:48:03 +0100

    Added first express code

---
+++ Part_2/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use(() => {
+
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
