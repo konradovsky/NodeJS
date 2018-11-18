a6b076dd7efbb1ea3accaa4556b539b03f388432
konradovsky <konrad.trade@gmail.com>
2018-11-19 00:37:46 +0100

    Adding routes with express

--- Part_2/app.js
+++ Part_2/app.js
@@ -4,8 +4,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('<h1>Test!</h1>'); // Just sends a response to the frontend
+app.use('/about', (req, res, next) => {
+    res.send('<h1>About page</h1>'); // Just sends a response to the frontend
+});
+app.use('/', (req, res, next) => {
+    res.send('<h1>Home Page</h1>'); // Just sends a response to the frontend
 });
 
 const server = http.createServer(app);
