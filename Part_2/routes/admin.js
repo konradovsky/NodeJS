4e7fe242fa339354f61b136f0d67c0e21484fde3
konradovsky <konrad.trade@gmail.com>
2018-11-19 22:38:44 +0100

    Added "Parents" routes for admin

--- Part_2/routes/admin.js
+++ Part_2/routes/admin.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Send</button></form>'); // Just sends a response to the frontend
+    res.send('<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type="submit">Send</button></form>'); // Just sends a response to the frontend
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
