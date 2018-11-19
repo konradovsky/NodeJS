492c013aa62ec0089e34bbe3af5d2a3b73cbf0b9
konradovsky <konrad.trade@gmail.com>
2018-11-19 18:43:34 +0100

    Parsing incoming requests
    With body parser

--- Part_2/app.js
+++ Part_2/app.js
@@ -1,12 +1,22 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
-app.use('/about', (req, res, next) => {
-    res.send('<h1>About page</h1>'); // Just sends a response to the frontend
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Send</button></form>'); // Just sends a response to the frontend
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
     res.send('<h1>Home Page</h1>'); // Just sends a response to the frontend
 });
