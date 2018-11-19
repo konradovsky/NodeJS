1127253acf6713ce1792b9284204b9e0a3af038c
konradovsky <konrad.trade@gmail.com>
2018-11-19 20:02:50 +0100

    Separating into routers

--- Part_2/app.js
+++ Part_2/app.js
@@ -1,26 +1,15 @@
 const http = require('http');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input name="title" type="text"><button type="submit">Send</button></form>'); // Just sends a response to the frontend
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Home Page</h1>'); // Just sends a response to the frontend
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
