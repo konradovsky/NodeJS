358ec3e27d46a951c6a4df50a4c204a1490bf7a4
konradovsky <konrad.trade@gmail.com>
2018-11-20 23:41:06 +0100

    Created GraphQL API

--- GraphQl/Part_1/src/index.js
+++ GraphQl/Part_1/src/index.js
@@ -1,1 +1,29 @@
-console.log('test');
+import { GraphQLServer} from 'graphql-yoga';
+
+// Type definitions / Application schema
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String
+    }
+`
+// Resorvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query'
+        },
+        name() {
+            return 'Konrad Efix'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The server is up');
+});
