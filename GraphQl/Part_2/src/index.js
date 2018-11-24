bb4433bc7706439a99151c29ffac55477d2552a0
konradovsky <konrad.trade@gmail.com>
2018-11-24 22:38:21 +0100

    Babel configuration
    Added npm i babel-plugin-transform-object-rest-spread

--- GraphQl/Part_2/src/index.js
+++ GraphQl/Part_2/src/index.js
@@ -142,9 +142,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user);
@@ -159,10 +157,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post)
@@ -182,9 +177,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text, 
-                author: args.text, 
-                post: args.text
+                ...args
             }
 
             comments.push(comment)
